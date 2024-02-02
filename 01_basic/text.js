6336268ae841b963c771b04f9de5b686612135d7
kapil35361 <121457707+kapil35361@users.noreply.github.com>
2024-02-02 07:10:23 +0000

    maths

--- 01_basic/text.js
+++ 01_basic/text.js
@@ -1,8 +1,17 @@
 let Name = '  kapil-rana'
-console.log(Name.trimStart())
-console.log(Name.splitt('-'))
-console.log(Name.reverse())
+let newName = Name.trimStart()
+console.log(newName.split('-'))
+// console.log(Name.reverse())
 let a = 10
 let b = 20
 let c = a+b
 console.log(`the sum of ${a} and ${b} is ${c}`)
+console.log(newName.charAt(8))
+console.log(newName.indexOf('k'))
+let substring = newName.substring(0,5)
+console.log(substring)
+console.log(newName.includes('kapil'))
+let score = 17383
+let newscore = new Number(1000)
+console.log(newscore)
+console.log(score.toFixed(2))
